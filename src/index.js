d0472d813ed37e310d3b195b49169f240a4e0573
konstantinstaykov <k.staykov@gmail.com>
2022-04-13 21:18:41 -0400

    HTTP Calls added mocking api, mocking library

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import {getUsers} from './api/userApi'
+import {getUsers, deleteUser} from './api/userApi'
 
 // Populate table of users via API call.
-getUsers().then(result=>{
+getUsers().then(result => {
   let usersBody = "";
 
-  result.forEach(user=>{
-    usersBody+=`<tr>
+  result.forEach(user => {
+    usersBody += `<tr>
 <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
 <td>${user.id}</td>
 <td>${user.firstName}</td>
@@ -14,4 +14,18 @@ getUsers().then(result=>{
 </tr>`
   });
   global.document.getElementById('users').innerHTML = usersBody;
-})
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  //Must use array.from to create a real array from DOM collection
+  //getElementByClassName only returns an "array like" object
+  Array.from(deleteLinks, link => {
+    link.onclick = function (event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    }
+  });
+});
